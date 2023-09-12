cca22db6aa56b0c787717a30656c5b35dcb5277f
Alibek20 <144598445+Alibek20@users.noreply.github.com>
2023-09-12 11:43:40 +0600

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.purple,),
-      body: Container(color: Colors.blue, child: Text("Adilbekov Alibek")),)
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue, 
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.green, 
+                padding: EdgeInsets.all(30.0),
+                margin: EdgeInsets.all(30.0),
+                child: Text(
+                  "Adil Talasbek",
+                  style: TextStyle(
+                    fontSize: 34,
+                    color: Colors.white, 
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.orange, 
+                child: Text(
+                  "ИС 20.810 digital marketing",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black, 
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
